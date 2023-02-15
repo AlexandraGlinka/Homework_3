8e3b7767c117975cacac50f2c00c247a7e4cdba1
glinka <alexandraglinka@yandex.ru>
2023-02-15 22:13:06 +0300

    Task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        //задача 1
+        byte a = 1;
+        short b = 100;
+        int c = 2345;
+        long d = 1_000_000;
+        float e = 1.15f;
+        double f = 29.01d;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+
 }
